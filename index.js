cf3dc3d14a4070a7a07e4b96d9dca74cab1e42ae
Titania01 <adegbitebidmus@gmail.com>
2022-04-14 01:11:52 +0100

    feat: created heroku

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ app.post('/api/persons', (req, res) => {
     return res.json(newPerson)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 }) 
